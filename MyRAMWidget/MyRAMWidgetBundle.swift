a389ed1cf7cacf71a5230c0f74577b045897e054
benjamindrong <38218923+benjamindrong@users.noreply.github.com>
2026-08-02 08:02:32 -0500

    MYR-197 Configure widget targets and App Groups

---
+++ MyRAMWidget/MyRAMWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MyRAMWidgetBundle.swift
+//  MyRAMWidget
+//
+//  Created by Benjamin Drong on 8/1/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MyRAMWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MyRAMWidget()
+    }
+}
